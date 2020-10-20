72f1da3b8c3a466c6937523035e5dc96ec76641f
Rémi Petitpierre <38254995+RPetitpierre@users.noreply.github.com>
2020-10-20 14:08:23 +0200

    Bug fixed: missing workshop folder

--- setup.py
+++ setup.py
@@ -40,6 +40,13 @@ setuptools.setup(
 
 import os
 
+
+os.makedirs('export', exist_ok=True)
+os.makedirs('workshop', exist_ok=True)
+os.makedirs('save', exist_ok=True)
+os.makedirs('data', exist_ok=True)
+os.makedirs('model', exist_ok=True)
+os.makedirs('settings', exist_ok=True)
 os.system('pip install --upgrade pip')
 os.system('conda update -n base -c defaults conda')
 os.system('conda create -n jadis python=3.6')
